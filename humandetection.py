648ca4d2bec8c97f7805ed0099e413b98a06728c
ryanyungnc <ryanyung@unc.edu>
2026-04-07 21:38:54 -0400

    Saving here

--- humandetection.py
+++ humandetection.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
+import torch
 from ultralytics import YOLO
+from sklearn.metrics.pairwise import cosine_similarity
 
+
+#def feature_analysis(frame):
 
 def video_capture(video_path):
     source = cv2.VideoCapture(video_path)
@@ -11,15 +15,15 @@ def video_capture(video_path):
     cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
     catalogued_ids = set()
-
+    
+    id_frame_counts = {}
+    
     while True:
         has_frame, frame = source.read()
         if not has_frame:
             break
 
-        results = model.track(frame, persist=True, verbose=False, classes=[0], tracker="bytetrack.yaml")
-        
-        id_frame_counts = {}
+        results = model.track(frame, persist=True, verbose=False, classes=[0], tracker="botsort.yaml", conf=0.5, iou=0.5)
 
         if results[0].boxes.id is not None:
 
@@ -28,16 +32,22 @@ def video_capture(video_path):
 
             for track_id, box in zip(track_ids, boxes):
                 
+                x1, y1, x2, y2 = box
+
+                crop = frame[y1:y2, x1:x2]
+
                 id_frame_counts[track_id] = id_frame_counts.get(track_id, 0) + 1
 
-                if id_frame_counts[track_id] and track_id not in catalogued_ids:
+                if id_frame_counts.get(track_id, 0) >= 7:
                     catalogued_ids.add(track_id)
 
                     print(f"New person detected! ID: {track_id}")
 
                     #could save crop of face here
+                else:
+                    pass
                 
-                x1, y1, x2, y2 = box
+                
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
                 cv2.putText(frame, f"ID: {track_id}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
@@ -51,4 +61,4 @@ def video_capture(video_path):
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    video_capture("MallCCTV.mp4")
+    video_capture("CCTV.mp4")
